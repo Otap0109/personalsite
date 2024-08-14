8c18f7cae8ce9cfc2055ba6594102379575e4740
Ostap Prusik <ostapprusik@gmail.com>
2024-08-14 15:11:35 +0200

    added grid

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./Grid.module.css"
+
+
+export default function Grid() {
+  return (
+    <div className={styles.div}><h1> Тут буде фото галерея </h1></div>
+  )
+}
--- src/components/Posluhy/Posluhy.tsx
+++ src/components/Posluhy/Posluhy.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import Image from "next/image";
 import styles from "./Posluhy.module.css";
 import { contentList } from "@/assets/lib/texts";
+import Grid from "../Grid/Grid";
 
 export default function Posluhy() {
   return (
     <main className={styles.wrapper} id="posluhy">
-{contentList.map((content, index) => (
-        <div key={content.key} className={styles.row} id={content.idname}>
-          {index % 2 === 0 ? (
-            <>
-              <div className={styles.image}>
-                {content.imageUrl}
-              </div>
-              <div>
-                <p>{content.description}</p>
-              </div>
-            </>
-          ) : (
-            <>
-              <div>
-                <p>{content.description}</p>
-              </div>
-              <div className={styles.image}>
-                {content.imageUrl}
-              </div>
-            </>
-          )}
+      {contentList.map((content, index) => (
+        <div key={content.key}>
+          <div className={styles.row} id={content.idname}>
+            {index % 2 === 0 ? (
+              <>
+                <div className={styles.image}>{content.imageUrl}</div>
+                <div>
+                  <p>{content.description}</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <p>{content.description}</p>
+                </div>
+                <div className={styles.image}>{content.imageUrl}</div>
+              </>
+            )}
+          </div>
+          {index === 1 && <Grid />}
         </div>
       ))}
     </main>
